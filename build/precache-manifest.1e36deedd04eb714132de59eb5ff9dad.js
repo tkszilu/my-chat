3586e727205ae142df3f8d346848a52c5b427c70
tkszilu <tkszilu@gmail.com>
2020-04-22 13:51:56 +0200

    build

---
+++ build/precache-manifest.1e36deedd04eb714132de59eb5ff9dad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7428c676ca6c7a0869920ee1369bc713",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7eb073a8c8a1c9ca92f2",
+    "url": "./static/css/main.4b98a952.chunk.css"
+  },
+  {
+    "revision": "894daed5cac13c915747",
+    "url": "./static/js/2.ee16ff66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ee16ff66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eb073a8c8a1c9ca92f2",
+    "url": "./static/js/main.dda9b28f.chunk.js"
+  },
+  {
+    "revision": "821e3e9e8623f25acdce",
+    "url": "./static/js/runtime-main.62724d6a.js"
+  }
+]);
